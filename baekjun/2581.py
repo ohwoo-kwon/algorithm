1ca32e2873377abdd292089a2d44ad87224efda1
ohwoo <kwonow1@gmail.com>
2022-04-03 14:30:22 +0900

    220403

--- baekjun/2581.py
+++ baekjun/2581.py
@@ -1,18 +1,29 @@
 M = int(input())
 N = int(input())
 
-count = 0
-numbers = []
+is_prime_num = [True] * (N+1)
+is_prime_num[0], is_prime_num[1] = False, False
+
+for num in range(2, int(N**0.5)+1):
+    if is_prime_num[num]:
+        temp = num + num
+        while temp <= N:
+            is_prime_num[temp] = False
+            temp += num
+
+
+sum_primes = 0
+min_value = 0
+flag = False
 for i in range(M, N+1):
-    if i == 1:
-        continue
-    for j in range(2, i):
-        if i % j == 0:
-            break
-    else:
-        count += i
-        numbers += [i]
-if count:
-    print(count, numbers[0])
+    if is_prime_num[i]:
+        sum_primes += i
+        if not flag:
+            min_value = i
+            flag = True
+
+if min_value:
+    print(sum_primes)
+    print(min_value)
 else:
     print(-1)
